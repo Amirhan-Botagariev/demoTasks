c12be964dbe9730de425d44c4ebc16aef6b61199
Amirhan Botagariev <ruby.mainacc@gmail.com>
2025-02-11 14:17:11 +0500

    Для первого задания на тему SQL создал таблицу и вставил в нее тестовые данные

---
+++ Workbench/Employees.sql
@@ -0,0 +1,26 @@
+-- Создание таблицы
+CREATE TABLE IF NOT EXISTS employees(
+    EmployeeId INT PRIMARY KEY,
+    ChiefId INT,
+    Department VARCHAR(50),
+    Name VARCHAR(50),
+    Salary DECIMAL(10,2),
+    FOREIGN KEY (ChiefId) REFERENCES employees(EmployeeId) ON DELETE SET NULL
+);
+
+-- Вставка данных
+INSERT INTO EMPLOYEES (EmployeeId, ChiefId, Department, Name, Salary) VALUES
+    (1, 1, 'Sales', 'Айгерим', 800),
+    (4, 4, 'Marketing', 'Сабина', 900),
+    (8, 8, 'QA', 'Дидар', 200);
+
+INSERT INTO EMPLOYEES (EmployeeId, ChiefId, Department, Name, Salary) VALUES
+    (2, 1, 'Sales', 'Диляра', 700),
+    (3, 4, 'Marketing', 'Мариям', 900),
+    (5, 4, 'Marketing', 'Мадина', 1000),
+    (6, 8, 'QA', 'Торгын', 300),
+    (7, 8, 'QA', 'Айжан', 200);
+
+
+
+
